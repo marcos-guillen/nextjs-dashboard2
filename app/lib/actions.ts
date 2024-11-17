2f46b3fbfe5326d753d11b69ccc250e5fee609a5
Marcos Guillen <10079595+marcos-guillen@users.noreply.github.com>
2024-11-17 01:50:50 +0100

    corregido error lint import error no se usa en app/lib/actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { error } from 'console';
+/* import { error } from 'console'; */
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
